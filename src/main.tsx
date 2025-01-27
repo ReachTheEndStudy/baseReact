5219e2f145b8afa1b89243238175ace573ca1e4e
ReachTheEnd <reachtheendstudy@gmail.com>
2025-01-27 20:52:31 +0300

    add lesson 10

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {App} from './App.tsx'
+import { App } from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const Counter = () => {
+  const [value, setValue] = useState(0)
+  const [isVisible, setIsVisible] = useState(true)
+
+  useEffect(() => {
+    console.log('Counter')
+    return () => {
+      console.log('return')
+    }
+  })
+
+
+  return <div>
+    <span>{value}</span>
+    <button onClick={() => setValue(v => v + 1)}>+1</button>
+    {/* <button onClick={() => setIsVisible(!isVisible)}>setIsVisible</button>
+    {isVisible && <Interval />} */}
+  </div>
+}
+
+// const Interval = () => {
+//   useEffect(() => {
+//     console.log('Interval')
+//     const inter = setInterval(() => {
+//       console.log('hello')
+//     }, 1000)
+
+//     return ()=>{
+//       clearInterval(inter)
+//     }
+//   }, [])
+
+//   return <div>hello</div>
+// }
+
+createRoot(document.getElementById('root')!).render(<Counter />)
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
