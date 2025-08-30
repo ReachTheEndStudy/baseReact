986e2a8f2e02ef53441d76a3747b065a75635393
ReachTheEndStudy <reachtheendstudy@gmail.com>
2025-08-30 18:34:13 +0300

    add lesson 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import style from './App.module.css';
 import { FieldWithAddButton } from './components/FieldWithAddButton/FieldWithAddButton';
 
+interface WeatherDataType {
+  location: {
+    name: string;
+  };
+  current: {
+    temp_c: number;
+    condition: {
+      text: string;
+      icon: string;
+    }
+  }
+}
+
 export function App() {
 
+  const [weatherData, setWeatherData] = useState<WeatherDataType | null>(null)
+
+  const onClickHandler = (city: string) => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=425af8de2ab647949c9165411252407&q=${city}`)
+      .then(res => {
+        res.json().then(res => setWeatherData(res))
+      })
+  }
+
+  console.log(weatherData)
+
   return <div className={style.wrapper}>
-    <FieldWithAddButton onClick={console.log} />
+    <FieldWithAddButton onClick={onClickHandler} />
+    {!!weatherData ? <div>
+      <div>{weatherData.location.name} - {weatherData.current.temp_c}</div>
+      <div>Погодные условия: {weatherData.current.condition.text} <img src={weatherData.current.condition.icon} /></div>
+
+    </div> : <div>Сделайте запрос</div>}
   </div>
 }
 
